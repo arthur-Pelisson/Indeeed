28afd606bdbdca6e3b25371936da4a28dfd1c7f5
arthur pelisson <arthur.pelisson@epitech.eu>
2021-09-01 08:41:07 +0200

    .env

--- Step04/api/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "cracotte666",
-    DB: "JobBoard"
-  };
--- Step05/api/routes/informations.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const informations = require("../controllers/informations.controller.js");
-  
-    // Create a new advertisement
-    app.post("/informations", informations.create);
-  };
